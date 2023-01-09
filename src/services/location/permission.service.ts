24ac4d6220cc306b2a7590fda5b60e1396f4214e
ypetrenko <vekaev4@gmail.com>
2023-01-09 18:02:23 +0100

    attempt to fix background permission #1

--- src/services/location/permission.service.ts
+++ src/services/location/permission.service.ts
@@ -1,9 +1,9 @@
 import { Alert } from 'react-native';
 import * as Location from 'expo-location';
-import { PermissionResponse } from 'expo-modules-core/src/PermissionsInterface';
+import { PermissionResponse, PermissionStatus } from 'expo-modules-core';
 
-const shouldRequestAgain = ({ canAskAgain, granted }: PermissionResponse) =>
-  !granted && canAskAgain;
+const shouldRequestAgain = ({ status }: PermissionResponse) =>
+  status !== PermissionStatus.GRANTED;
 
 export const isLocationPermissionGranted = async (): Promise<boolean> => {
   try {
@@ -13,21 +13,22 @@ export const isLocationPermissionGranted = async (): Promise<boolean> => {
       foregroundPermission = await Location.requestForegroundPermissionsAsync();
     }
 
+    if (foregroundPermission.status !== PermissionStatus.GRANTED)
+      throw new Error('Foreground permission not granted');
+
     let backgroundPermission = await Location.getBackgroundPermissionsAsync();
 
     if (shouldRequestAgain(backgroundPermission)) {
       backgroundPermission = await Location.requestBackgroundPermissionsAsync();
     }
 
-    if (!backgroundPermission.granted) {
-      Alert.alert(
-        'BackgroundPermission',
-        JSON.stringify(backgroundPermission, null, 2),
-      );
-    }
-
     return foregroundPermission?.granted && backgroundPermission?.granted;
   } catch (error) {
+    Alert.alert(
+      'Something went wrong while requesting permissions',
+      JSON.stringify(error, null, 2),
+    );
+
     return false;
   }
 };
